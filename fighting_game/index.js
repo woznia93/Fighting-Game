92323dd4876d5f07b06bc1e4c4c9b90f2b17033c
Brady <woznia93@msu.edu>
2024-11-23 03:25:23 -0500

    Addding sprites, background won't update to picture

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -12,66 +12,18 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7 // is .7 in refernce but fast fall is on 
 
-class Sprite {
-    constructor({position, velocity, color = 'red', offset}) {
-        this.position = position
-        this.velocity = velocity 
-        this.width = 50
-        this.height = 150
-        this.lastKeyx
-        this.lastKeyy
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-        }
-        this.color = color 
-        this.isAttacking
-        this.health = 100
-    }
-
-    draw() {
-        c.fillStyle = this.color 
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
 
-        // attack box 
-        if (this.isAttacking) {
-            c.fillStyle = 'white'
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        }
-
-    }
-
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        this.position.x += this.velocity.x
- 
-        this.position.y += this.velocity.y 
-
-        if (this.position.y + this.height +this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else this.velocity.y += gravity
-    }
-
-    attack() {
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100)
-    }
-
-}
+const background = new Sprite({
+    positon: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/hillsbackground.png'
+})
 
 
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -87,7 +39,7 @@ const player = new Sprite({
 })
 
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 500,
         y: 100
@@ -178,6 +130,8 @@ function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+
+
     player.update()
     enemy.update()
 
